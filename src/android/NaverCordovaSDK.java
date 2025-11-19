79e35a9a021a5cee37c162ba9ac5c892b9158832
kimil144 <kimil144@naver.com>
2025-11-19 18:03:32 +0900

    Revert "1. 네이버 로그인 수정"
    This reverts commit 111d8b24dcf121b681c8d146a8950a035f50e529.

--- src/android/NaverCordovaSDK.java
+++ src/android/NaverCordovaSDK.java
@@ -1,19 +1,14 @@
 package com.raccoondev85.plugin.naver;
 
-import android.content.Context;
-
-import com.navercorp.nid.oauth.NidOAuth;
-import com.navercorp.nid.oauth.NidOAuthCallback;
-import com.navercorp.nid.oauth.NidOAuthInitializingCallback;
+import com.navercorp.nid.NaverIdLoginSDK;
+import com.navercorp.nid.oauth.NidOAuthLogin;
+import com.navercorp.nid.oauth.OAuthLoginCallback;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.concurrent.atomic.AtomicBoolean;
-
 
 
 public class NaverCordovaSDK extends CordovaPlugin {
@@ -22,118 +17,241 @@ public class NaverCordovaSDK extends CordovaPlugin {
     private static final boolean DEBUG_LOG = true;
 //    private OAuthLogin NaverIdLoginSDK.INSTANCE;
 
-     @Override
+    @Override
     protected void pluginInitialize() {
         super.pluginInitialize();
-        final Context app = cordova.getActivity().getApplicationContext();
 
-        // SDK 초기화 (5.11.x)
-        if (sInitialized.compareAndSet(false, true)) {
-            // 리소스 로딩 (기존 그대로)
-            NaverResources.initResources(app);
+        try{
+            NaverResources.initResources(cordova.getActivity().getApplication());
+            NaverIdLoginSDK.INSTANCE.initialize(cordova.getActivity().getApplication(), NaverResources.OAUTH_CLIENT_ID, NaverResources.OAUTH_CLIENT_SECRET, NaverResources.OAUTH_CLIENT_NAME);
+//            NaverResources.initResources(cordova.getActivity().getApplication());
+//            NaverIdLoginSDK.INSTANCE = OAuthLogin.getInstance();
+//            NaverIdLoginSDK.INSTANCE.showDevelopersLog(DEBUG_LOG);
+//            NaverIdLoginSDK.INSTANCE.init(cordova.getActivity(), NaverResources.OAUTH_CLIENT_ID,
+//                    NaverResources.OAUTH_CLIENT_SECRET, NaverResources.OAUTH_CLIENT_NAME);
+        }catch (Exception e) {
 
-            NidOAuth.initialize(
-                app,
-                NaverResources.OAUTH_CLIENT_ID,
-                NaverResources.OAUTH_CLIENT_SECRET,
-                NaverResources.OAUTH_CLIENT_NAME,
-                new NidOAuthInitializingCallback() {
-                    @Override public void onSuccess() { /* 필요시 로그 */ }
-                    @Override public void onFailure(Exception e) { /* 필요시 로그 */ }
-                }
-            );
-            NidOAuth.setShowDevelopersLog(DEBUG_LOG);
         }
     }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        switch (action) {
-            case "login":              login(callbackContext);            return true;
-            case "logout":             logout(callbackContext);           return true;
-            case "unlinkApp":          unlinkApp(callbackContext);        return true;
-            case "refreshAccessToken": refreshAccessToken(callbackContext); return true;
-            case "getAccessToken":     getAccessToken(callbackContext);   return true;
+        if (action.equals("login")) {
+            this.login(callbackContext);
+            return true;
+        } else if (action.equals("logout")) {
+            this.logout(callbackContext);
+            return true;
+        } else if (action.equals("unlinkApp")) {
+            this.unlinkApp(callbackContext);
+            return true;
+        } else if (action.equals("refreshAccessToken")) {
+            this.refreshAccessToken(callbackContext);
+            return true;
+        } else if (action.equals("getAccessToken")) {
+            this.getAccessToken(callbackContext);
+            return true;
         }
+
         return false;
     }
 
-    private void login(final CallbackContext cb) {
-        cordova.getActivity().runOnUiThread(() ->
-            NidOAuth.requestLogin(cordova.getActivity(), new NidOAuthCallback() {
-                @Override public void onSuccess() {
-                    try {
+
+    private void login(final CallbackContext callbackContext) {
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                NaverIdLoginSDK.INSTANCE.authenticate(cordova.getActivity(), new OAuthLoginCallback() {
+
+                    @Override
+                    public void onSuccess() {
+                        String accessToken = NaverIdLoginSDK.INSTANCE.getAccessToken();
+                        String refreshToken = NaverIdLoginSDK.INSTANCE.getRefreshToken();
+                        long expiresAt = NaverIdLoginSDK.INSTANCE.getExpiresAt();
+                        String tokenType = NaverIdLoginSDK.INSTANCE.getTokenType();
+
                         JSONObject userinfo = new JSONObject();
-                        userinfo.put("accessToken",  NidOAuth.getAccessToken());
-                        userinfo.put("refreshToken", NidOAuth.getRefreshToken());
-                        userinfo.put("expiresAt",    NidOAuth.getExpiresAt());
-                        userinfo.put("tokenType",    NidOAuth.getTokenType());
-                        cb.success(userinfo);
-                    } catch (JSONException e) {
-                        cb.error(e.getMessage());
+                        try {
+                            userinfo.put("accessToken", accessToken);
+                            userinfo.put("refreshToken", refreshToken);
+                            userinfo.put("expiresAt", expiresAt);
+                            userinfo.put("tokenType", tokenType);
+
+                            callbackContext.success(userinfo);
+
+//                            JSONObject prop = new JSONObject(content.toString());
+//                            JSONObject[] objs = new JSONObject[] { userinfo, (JSONObject) prop.get("response") };
+//                            for (JSONObject obj : objs) {
+//                                Iterator it = obj.keys();
+//                                while (it.hasNext()) {
+//                                    String key = (String)it.next();
+//                                    resultObject.put(key, obj.get(key));
+//                                }
+//                            }
+//                            mCallbackContext.success(resultObject);
+                        } catch (JSONException e) {
+                            callbackContext.error(e.getMessage());
+                        }
                     }
-                }
-                @Override public void onFailure(String errorCode, String errorDesc) {
-                    try {
-                        JSONObject err = new JSONObject();
-                        err.put("code", errorCode);
-                        err.put("description", errorDesc);
-                        cb.error(err);
-                    } catch (JSONException e) {
-                        cb.error(e.getMessage());
+
+                    @Override
+                    public void onFailure(int httpStatus, String message) {
+                        String errorCode = NaverIdLoginSDK.INSTANCE.getLastErrorCode().getCode();
+                        String errorDescription = NaverIdLoginSDK.INSTANCE.getLastErrorDescription();
+
+                        JSONObject resultObject = new JSONObject();
+                        try {
+                            resultObject.put("code", errorCode);
+                            resultObject.put("description", errorDescription);
+
+                            callbackContext.error(resultObject);
+                        } catch (JSONException e) {
+                            callbackContext.error(e.getMessage());
+                        }
                     }
+
+                    @Override
+                    public void onError(int errorCode, String message) {
+                        onFailure(errorCode, message);
+                    }
+                });
+            }
+        });
+
+    }
+
+
+    private void logout(CallbackContext callbackContext) {
+        NaverIdLoginSDK.INSTANCE.logout();
+        callbackContext.success();
+    }
+
+
+    private void unlinkApp(CallbackContext callbackContext) {
+
+        NidOAuthLogin login = new NidOAuthLogin();
+        login.callDeleteTokenApi(cordova.getActivity(), new OAuthLoginCallback() {
+            @Override
+            public void onSuccess() {
+                callbackContext.success();
+           }
+
+            @Override
+            public void onFailure(int httpStatus, String message) {
+                String errorCode = NaverIdLoginSDK.INSTANCE.getLastErrorCode().getCode();
+                String errorDescription = NaverIdLoginSDK.INSTANCE.getLastErrorDescription();
+
+                JSONObject resultObject = new JSONObject();
+                try {
+                    resultObject.put("code", errorCode);
+                    resultObject.put("description", errorDescription);
+
+                    callbackContext.error(resultObject);
+                } catch (JSONException e) {
+                    callbackContext.error(e.getMessage());
                 }
-            })
-        );
-    }
+           }
 
-    private void logout(final CallbackContext cb) {
-        NidOAuth.logout(new NidOAuthCallback() {
-            @Override public void onSuccess() { cb.success(); }
-            @Override public void onFailure(String code, String desc) {
-                try {
-                    JSONObject err = new JSONObject();
-                    err.put("code", code);
-                    err.put("description", desc);
-                    cb.error(err);
-                } catch (JSONException e) { cb.error(e.getMessage()); }
+            @Override
+            public void onError(int errorCode, String message) {
+                onFailure(errorCode, message);
             }
         });
     }
 
-    private void unlinkApp(final CallbackContext cb) {
-        // 연동 해제 (서버 토큰 삭제 + 클라이언트 토큰 삭제)
-        NidOAuth.disconnect(new NidOAuthCallback() {
-            @Override public void onSuccess() { cb.success(); }
-            @Override public void onFailure(String code, String desc) {
+
+    private void refreshAccessToken(CallbackContext callbackContext) {
+        NidOAuthLogin login = new NidOAuthLogin();
+        login.callRefreshAccessTokenApi(cordova.getActivity(), new OAuthLoginCallback() {
+            @Override
+            public void onSuccess() {
+                callbackContext.success(NaverIdLoginSDK.INSTANCE.getAccessToken());
+            }
+
+            @Override
+            public void onFailure(int httpStatus, String message) {
+                String errorCode = NaverIdLoginSDK.INSTANCE.getLastErrorCode().getCode();
+                String errorDescription = NaverIdLoginSDK.INSTANCE.getLastErrorDescription();
+
+                JSONObject resultObject = new JSONObject();
                 try {
-                    JSONObject err = new JSONObject();
-                    err.put("code", code);
-                    err.put("description", desc);
-                    cb.error(err);
-                } catch (JSONException e) { cb.error(e.getMessage()); }
+                    resultObject.put("code", errorCode);
+                    resultObject.put("description", errorDescription);
+
+                    callbackContext.error(resultObject);
+                } catch (JSONException e) {
+                    callbackContext.error(e.getMessage());
+                }
+            }
+
+            @Override
+            public void onError(int errorCode, String message) {
+                onFailure(errorCode, message);
             }
         });
     }
 
-    private void refreshAccessToken(final CallbackContext cb) {
-        // 5.11.x에서는 requestLogin이 토큰 갱신 플로우를 포함
-        NidOAuth.requestLogin(cordova.getActivity(), new NidOAuthCallback() {
-            @Override public void onSuccess() {
-                cb.success(NidOAuth.getAccessToken());
-            }
-            @Override public void onFailure(String code, String desc) {
-                try {
-                    JSONObject err = new JSONObject();
-                    err.put("code", code);
-                    err.put("description", desc);
-                    cb.error(err);
-                } catch (JSONException e) { cb.error(e.getMessage()); }
-            }
-        });
+
+    private void getAccessToken(CallbackContext callbackContext) {
+        String accessToken = NaverIdLoginSDK.INSTANCE.getAccessToken();
+        callbackContext.success(accessToken);
     }
 
-    private void getAccessToken(CallbackContext cb) {
-        cb.success(NidOAuth.getAccessToken());
-    }
+
+//    private class RequestApiTask extends AsyncTask<Object, Void, String> {
+//        private CallbackContext mCallbackContext;
+//        private Context mContext;
+//
+//        @Override
+//        protected String doInBackground(Object... args) {
+//            mContext = (Context) args[0];
+//            mCallbackContext = (CallbackContext)args[1];
+//
+//            String url = "https://openapi.naver.com/v1/nid/me";
+//            String at = NaverIdLoginSDK.INSTANCE.getAccessToken();
+//            return NaverIdLoginSDK.INSTANCE.requestApi(mContext, at, url);
+//        }
+//
+//        @Override
+//        protected void onPreExecute() {
+//
+//        }
+//
+//
+//        protected void onPostExecute(String content) {
+//            if(content == null){
+//                mCallbackContext.error("API call failed");
+//                return;
+//            }
+//            JSONObject resultObject = new JSONObject();
+//
+//            try{
+//                String accessToken = NaverIdLoginSDK.INSTANCE.getAccessToken(mContext);
+//                String refreshToken = NaverIdLoginSDK.INSTANCE.getRefreshToken(mContext);
+//                long expiresAt = NaverIdLoginSDK.INSTANCE.getExpiresAt(mContext);
+//                String tokenType = NaverIdLoginSDK.INSTANCE.getTokenType(mContext);
+//
+//                JSONObject userinfo = new JSONObject();
+//
+//                userinfo.put("accessToken", accessToken);
+//                userinfo.put("refreshToken", refreshToken);
+//                userinfo.put("expiresAt", expiresAt);
+//                userinfo.put("tokenType", tokenType);
+//
+//                JSONObject prop = new JSONObject(content.toString());
+//                JSONObject[] objs = new JSONObject[] { userinfo, (JSONObject) prop.get("response") };
+//                for (JSONObject obj : objs) {
+//                    Iterator it = obj.keys();
+//                    while (it.hasNext()) {
+//                        String key = (String)it.next();
+//                        resultObject.put(key, obj.get(key));
+//                    }
+//                }
+//                mCallbackContext.success(resultObject);
+//            }catch (Exception e){
+//                mCallbackContext.error(e.getMessage());
+//            }
+//
+//        }
+//    }
 }
